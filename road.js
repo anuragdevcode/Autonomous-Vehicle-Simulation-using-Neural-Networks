0a8644e219011ee3a427f6ba092040143fb94ed3
anuragdevcode <anuragks.codes@gmail.com>
2026-01-01 20:06:30 +0530

    fixed the bug in road.js

--- road.js
+++ road.js
@@ -11,7 +11,7 @@ class Road{
         this.top = -infintiy;
         this.bottom = infintiy;
         const topLeft ={x:this.left,y:this.top};
-        const topRight ={x:this.left,y:this.top};
+        const topRight ={x:this.right,y:this.top};
         const bottomLeft ={x:this.left,y:this.bottom};
         const bottomRight ={x:this.right,y:this.bottom};
         this.borders = [
