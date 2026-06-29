018a2c2cb91d5ca23592538c1a4cd114eb2ffdc2
anuragdevcode <anuragks.codes@gmail.com>
2026-06-29 17:47:08 +0530

    Added NueralNetwork and connected it to the car controls.

--- car.js
+++ car.js
@@ -11,8 +11,10 @@ class Car{
         this.maxSpeed = 3;
         this.friction = 0.05
         this.angle = 0;
+        this.useBrain = controlType == "AI";
         if(controlType != "DUMMY"){
         this.sensor = new Sensor(this);// sensor is added
+        this.brain = new NeuralNetwork([this.sensor.rayCount,6,4]); // neural network is added
         }
         this.controls = new Controls(controlType);
         this.damaged = false;
@@ -28,6 +30,16 @@ class Car{
         }
         if(this.sensor){
             this.sensor.update(roadBorders,traffic);
+            const offsets = this.sensor.readings.map(
+                s => s == null ? 0 : 1-s.offset
+            );
+            const outputs = NeuralNetwork.feedForward(offsets,this.brain);
+            if(this.useBrain){
+                this.controls.forward = outputs[0];
+                this.controls.left = outputs[1];
+                this.controls.right = outputs[2];
+                this.controls.reverse = outputs[3];
+            }
         }
     }
 
--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-const canvas = document.getElementById("myCanvas");
-
-canvas.width = 200;
-const ctx = canvas.getContext("2d")
-const road = new Road(canvas.width/2,canvas.width*0.9);
-const car = new Car(road.getLaneCenter(1),100,30,50,"KEYS");
+const carCanvas = document.getElementById("carCanvas");
+carCanvas.width = 200;
+const networkCanvas = document.getElementById("networkCanvas");
+networkCanvas.width = 300;
+const carCtx = carCanvas.getContext("2d")
+const networkCtx = networkCanvas.getContext("2d")
+const road = new Road(carCanvas.width/2,carCanvas.width*0.9);
+const car = new Car(road.getLaneCenter(1),100,30,50,"AI");
 const traffic = [
     new Car(road.getLaneCenter(1),-100,30,50,"DUMMY"),
+    new Car(road.getLaneCenter(0),-300,30,50,"DUMMY"),
+    new Car(road.getLaneCenter(2),-300,30,50,"DUMMY"),
+    new Car(road.getLaneCenter(1),-500,30,50,"DUMMY"),
 ];
 animate();
 function animate(){
@@ -13,14 +18,15 @@ function animate(){
         traffic[i].update(road.borders,[]);
     }
     car.update(road.borders,traffic);
-    canvas.height = window.innerHeight;
-    ctx.save();
-    ctx.translate(0,-car.y+canvas.height*0.7);
-    road.draw(ctx);
+    carCanvas.height = window.innerHeight;
+    networkCanvas.height = window.innerHeight;
+    carCtx.save();
+    carCtx.translate(0,-car.y+carCanvas.height*0.7);
+    road.draw(carCtx);
     for(let i = 0; i<traffic.length; i++){
-        traffic[i].draw(ctx);
+        traffic[i].draw(carCtx);
     }
-     car.draw(ctx);
-    ctx.restore();
+     car.draw(carCtx);
+    carCtx.restore();
     requestAnimationFrame(animate);
 }
